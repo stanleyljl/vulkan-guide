c7a1aae71613b7f496b3d53457e81fe6ed5db49e
stanleyljl <798080536@qq.com>
2023-03-21 15:55:52 +0800

    rename package

--- android/proj/app/src/main/java/com/android/example/MainActivity.java
+++ android/proj/app/src/main/java/com/android/example/MainActivity.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.android.example.vulkan.tutorials.five;
+package com.android.example;
 
 import com.google.androidgamesdk.GameActivity;
 import android.os.Bundle;
